95eefcd47e687ae2cf2bba3ab41216889cf25d58
Sinem Göçtü <goctusinem@gmail.com>
2025-05-29 15:01:33 +0300

    Added the logic for http response status codes.

--- router.php
+++ router.php
@@ -8,6 +8,16 @@ function routeToController($uri, $routes) {
     if (array_key_exists($uri, $routes)) {
         require $routes[$uri];
     }
+    else {
+        abort();
+    }
+}
+
+function abort($status_code = 404) {
+    http_response_code($status_code);
+
+    require "views/$status_code.php";
+    die();
 }
 
 routeToController($uri, $routes);
