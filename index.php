be09598a702c5d7f8abf611c7b64f6088b0d6e1a
Sinem Göçtü <goctusinem@gmail.com>
2025-05-29 18:00:24 +0300

    Added the logic to prevent SQL injection.

--- index.php
+++ index.php
@@ -7,5 +7,11 @@ require 'Database.php';
 $config = require('config.php');
 $db = new Database($config['database']);
 
-$tasks = $db->query(/** @lang text */ "select * from tasks")->fetchAll();
+$id = $_GET['id'];
+
+//Prevent sql injection using wildcards
+$query = /** @lang text */
+    "select * from tasks where id = :id";
+
+$tasks = $db->query($query, [':id' => $id])->fetch();
 dd($tasks);
