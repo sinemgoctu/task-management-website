3f6e1f10c6bebab60a22b418e1e093687de0e84c
Sinem Göçtü <goctusinem@gmail.com>
2025-05-29 14:45:36 +0300

    Added router, organized files.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 
-require "views/index.view.php";
+require "functions.php";
 
+require "router.php";
---
+++ router.php
@@ -0,0 +1,13 @@
+<?php
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = ["/" => "controllers/index.php"];
+
+function routeToController($uri, $routes) {
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    }
+}
+
+routeToController($uri, $routes);
